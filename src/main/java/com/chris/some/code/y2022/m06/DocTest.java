e68dac1ef860b06d7ea5b33805a2fa031bfde4bc
gaoruiting <oi.yi@outlook.com>
2022-06-10 16:16:16 +0800

    🚧doc pdf

---
+++ src/main/java/com/chris/some/code/y2022/m06/DocTest.java
@@ -0,0 +1,34 @@
+package com.chris.some.code.y2022.m06;
+
+import java.io.IOException;
+
+/**
+ * <dependency>
+ * <groupId>org.apache.poi</groupId>
+ * <artifactId>poi</artifactId>
+ * <version>4.1.2</version>
+ * </dependency>
+ *
+ * <dependency>
+ * <groupId>com.deepoove</groupId>
+ * <artifactId>poi-tl</artifactId>
+ * <version>1.10.0</version>
+ * </dependency>
+ *
+ * @author grt
+ */
+public class DocTest {
+
+    public static void main(String[] args) throws IOException {
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name",
+//                Texts.of("放得开撒娇反馈放得开撒娇弗兰克撒大家风口浪尖阿克琉斯九分裤两件啊圣诞快乐方静看大家反馈啦健康连锁酒店就撒开积分卡家")
+//                        .color("000000")
+//                        .create()
+//        );
+//        XWPFTemplate template = XWPFTemplate.compile("/Users/alex/Downloads/劳动合同书.docx").render(map);
+//        List<MetaTemplate> elementTemplates = template.getElementTemplates();
+//        elementTemplates.forEach(metaTemplate -> System.out.println(metaTemplate.variable()));
+//        template.writeAndClose(new FileOutputStream("/Users/alex/Downloads/劳动合同书（生成）.pdf"));
+    }
+}
---
+++ src/main/java/com/chris/some/code/y2022/m06/PdfTest.java
@@ -0,0 +1,52 @@
+package com.chris.some.code.y2022.m06;
+
+import java.io.IOException;
+
+/**
+ * <repository>
+ * <id>AsposeJavaAPI</id>
+ * <name>Aspose Java API</name>
+ * <url>http://repository.aspose.com/repo/</url>
+ * </repository>
+ *
+ * <dependency>
+ * <groupId>com.aspose</groupId>
+ * <artifactId>aspose-pdf</artifactId>
+ * <version>22.5</version>
+ * </dependency>
+ * <dependency>
+ * <groupId>javax.media.jai</groupId>
+ * <artifactId>com.springsource.javax.media.jai.core</artifactId>
+ * <version>1.1.3</version>
+ * </dependency>
+ * <dependency>
+ * <groupId>com.aspose</groupId>
+ * <artifactId>aspose-words</artifactId>
+ * <version>19.12</version>
+ * <classifier>jdk17</classifier>
+ * </dependency>
+ * <!-- https://mvnrepository.com/artifact/commons-io/commons-io -->
+ * <dependency>
+ * <groupId>commons-io</groupId>
+ * <artifactId>commons-io</artifactId>
+ * <version>2.7</version>
+ * </dependency>
+ *
+ * @author grt
+ */
+public class PdfTest {
+
+    public static void main(String[] args) throws IOException {
+        // aspose.pdf
+//        Document document = new Document("/Users/alex/Downloads/AAA.pdf");
+//        TextFragmentAbsorber textFragmentAbsorber = new TextFragmentAbsorber("\\w{3,16}");
+//        document.getPages().accept(textFragmentAbsorber);
+//        TextFragmentCollection textFragments = textFragmentAbsorber.getTextFragments();
+//        for (TextFragment textFragment : textFragments) {
+//            String text = textFragment.getText();
+//            System.out.println("text = " + text);
+//            textFragment.setText("疯狂撒娇反馈绝对是方静都是会计法");
+//        }
+//        document.save("/Users/alex/Downloads/AAA(create).pdf");
+    }
+}
