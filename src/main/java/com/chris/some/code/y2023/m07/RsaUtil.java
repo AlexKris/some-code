ef8e5fc814e31255161c8a4fb8e45002522b3380
grt <thoth.g05@gmail.com>
2023-07-07 15:19:06 +0800

    RSA

--- src/main/java/com/chris/some/code/y2023/m07/RsaUtil.java
+++ src/main/java/com/chris/some/code/y2023/m07/RsaUtil.java
@@ -10,14 +10,20 @@ import java.util.Map;
 
 public class RsaUtil {
 
-    //签名算法名称
+    /**
+     * 签名算法名称
+     */
     private static final String RSA_KEY_ALGORITHM = "RSA";
 
-    //标准签名算法名称
+    /**
+     * 标准签名算法名称
+     */
     private static final String RSA_SIGNATURE_ALGORITHM = "SHA1withRSA";
     private static final String RSA2_SIGNATURE_ALGORITHM = "SHA256withRSA";
 
-    //RSA密钥长度,默认密钥长度是1024,密钥长度必须是64的倍数，在512到65536位之间,不管是RSA还是RSA2长度推荐使用2048
+    /**
+     * RSA密钥长度,默认密钥长度是1024,密钥长度必须是64的倍数，在512到65536位之间,不管是RSA还是RSA2长度推荐使用2048
+     */
     private static final int KEY_SIZE = 2048;
 
     /**
@@ -33,18 +39,18 @@ public class RsaUtil {
             throw new RuntimeException("RSA初始化密钥出现错误,算法异常");
         }
         SecureRandom secrand = new SecureRandom();
-        //初始化随机产生器
+        // 初始化随机产生器
         secrand.setSeed("Alian".getBytes());
-        //初始化密钥生成器
+        // 初始化密钥生成器
         keygen.initialize(KEY_SIZE, secrand);
         KeyPair keyPair = keygen.genKeyPair();
-        //获取公钥并转成base64编码
+        // 获取公钥并转成base64编码
         byte[] pub_key = keyPair.getPublic().getEncoded();
         String publicKeyStr = Base64.getEncoder().encodeToString(pub_key);
-        //获取私钥并转成base64编码
+        // 获取私钥并转成base64编码
         byte[] pri_key = keyPair.getPrivate().getEncoded();
         String privateKeyStr = Base64.getEncoder().encodeToString(pri_key);
-        //创建一个Map返回结果
+        // 创建一个Map返回结果
         Map<String, String> keyPairMap = new HashMap<>();
         keyPairMap.put("publicKeyStr", publicKeyStr);
         keyPairMap.put("privateKeyStr", privateKeyStr);
@@ -60,21 +66,21 @@ public class RsaUtil {
      * @throws Exception 异常
      */
     public static String encryptByPublicKey(String data, String publicKeyStr) throws Exception {
-        //Java原生base64解码
+        // Java原生base64解码
         byte[] pubKey = Base64.getDecoder().decode(publicKeyStr);
-        //创建X509编码密钥规范
+        // 创建X509编码密钥规范
         X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(pubKey);
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //根据X509编码密钥规范产生公钥对象
+        // 根据X509编码密钥规范产生公钥对象
         PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
-        //根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
+        // 根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
         Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        //用公钥初始化此Cipher对象（加密模式）
+        // 用公钥初始化此Cipher对象（加密模式）
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        //对数据加密
+        // 对数据加密
         byte[] encrypt = cipher.doFinal(data.getBytes());
-        //返回base64编码后的字符串
+        // 返回base64编码后的字符串
         return Base64.getEncoder().encodeToString(encrypt);
     }
 
@@ -87,21 +93,21 @@ public class RsaUtil {
      * @throws Exception 异常
      */
     public static String decryptByPrivateKey(String data, String privateKeyStr) throws Exception {
-        //Java原生base64解码
+        // Java原生base64解码
         byte[] priKey = Base64.getDecoder().decode(privateKeyStr);
-        //创建PKCS8编码密钥规范
+        // 创建PKCS8编码密钥规范
         PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(priKey);
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //根据PKCS8编码密钥规范产生私钥对象
+        // 根据PKCS8编码密钥规范产生私钥对象
         PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
-        //根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
+        // 根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
         Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        //用私钥初始化此Cipher对象（解密模式）
+        // 用私钥初始化此Cipher对象（解密模式）
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        //对数据解密
+        // 对数据解密
         byte[] decrypt = cipher.doFinal(Base64.getDecoder().decode(data));
-        //返回字符串
+        // 返回字符串
         return new String(decrypt);
     }
 
@@ -114,21 +120,21 @@ public class RsaUtil {
      * @throws Exception 异常
      */
     public static String encryptByPrivateKey(String data, String privateKeyStr) throws Exception {
-        //Java原生base64解码
+        // Java原生base64解码
         byte[] priKey = Base64.getDecoder().decode(privateKeyStr);
-        //创建PKCS8编码密钥规范
+        // 创建PKCS8编码密钥规范
         PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(priKey);
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //根据PKCS8编码密钥规范产生私钥对象
+        // 根据PKCS8编码密钥规范产生私钥对象
         PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
-        //根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
+        // 根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
         Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        //用私钥初始化此Cipher对象（加密模式）
+        // 用私钥初始化此Cipher对象（加密模式）
         cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-        //对数据加密
+        // 对数据加密
         byte[] encrypt = cipher.doFinal(data.getBytes());
-        //返回base64编码后的字符串
+        // 返回base64编码后的字符串
         return Base64.getEncoder().encodeToString(encrypt);
     }
 
@@ -141,21 +147,21 @@ public class RsaUtil {
      * @throws Exception 异常
      */
     public static String decryptByPublicKey(String data, String publicKeyStr) throws Exception {
-        //Java原生base64解码
+        // Java原生base64解码
         byte[] pubKey = Base64.getDecoder().decode(publicKeyStr);
-        //创建X509编码密钥规范
+        // 创建X509编码密钥规范
         X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(pubKey);
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //根据X509编码密钥规范产生公钥对象
+        // 根据X509编码密钥规范产生公钥对象
         PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
-        //根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
+        // 根据转换的名称获取密码对象Cipher（转换的名称：算法/工作模式/填充模式）
         Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        //用公钥初始化此Cipher对象（解密模式）
+        // 用公钥初始化此Cipher对象（解密模式）
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
-        //对数据解密
+        // 对数据解密
         byte[] decrypt = cipher.doFinal(Base64.getDecoder().decode(data));
-        //返回字符串
+        // 返回字符串
         return new String(decrypt);
     }
 
@@ -169,23 +175,23 @@ public class RsaUtil {
      * @throws Exception 异常
      */
     public static String sign(byte[] data, byte[] priKey, String signType) throws Exception {
-        //创建PKCS8编码密钥规范
+        // 创建PKCS8编码密钥规范
         PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(priKey);
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //根据PKCS8编码密钥规范产生私钥对象
+        // 根据PKCS8编码密钥规范产生私钥对象
         PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
-        //标准签名算法名称(RSA还是RSA2)
+        // 标准签名算法名称(RSA还是RSA2)
         String algorithm = RSA_KEY_ALGORITHM.equals(signType) ? RSA_SIGNATURE_ALGORITHM : RSA2_SIGNATURE_ALGORITHM;
-        //用指定算法产生签名对象Signature
+        // 用指定算法产生签名对象Signature
         Signature signature = Signature.getInstance(algorithm);
-        //用私钥初始化签名对象Signature
+        // 用私钥初始化签名对象Signature
         signature.initSign(privateKey);
-        //将待签名的数据传送给签名对象(须在初始化之后)
+        // 将待签名的数据传送给签名对象(须在初始化之后)
         signature.update(data);
-        //返回签名结果字节数组
+        // 返回签名结果字节数组
         byte[] sign = signature.sign();
-        //返回Base64编码后的字符串
+        // 返回Base64编码后的字符串
         return Base64.getEncoder().encodeToString(sign);
     }
 
@@ -199,21 +205,21 @@ public class RsaUtil {
      * @return boolean 校验成功返回true，失败返回false
      */
     public static boolean verify(byte[] data, byte[] sign, byte[] pubKey, String signType) throws Exception {
-        //返回转换指定算法的KeyFactory对象
+        // 返回转换指定算法的KeyFactory对象
         KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
-        //创建X509编码密钥规范
+        // 创建X509编码密钥规范
         X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(pubKey);
-        //根据X509编码密钥规范产生公钥对象
+        // 根据X509编码密钥规范产生公钥对象
         PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
-        //标准签名算法名称(RSA还是RSA2)
+        // 标准签名算法名称(RSA还是RSA2)
         String algorithm = RSA_KEY_ALGORITHM.equals(signType) ? RSA_SIGNATURE_ALGORITHM : RSA2_SIGNATURE_ALGORITHM;
-        //用指定算法产生签名对象Signature
+        // 用指定算法产生签名对象Signature
         Signature signature = Signature.getInstance(algorithm);
-        //用公钥初始化签名对象,用于验证签名
+        // 用公钥初始化签名对象,用于验证签名
         signature.initVerify(publicKey);
-        //更新签名内容
+        // 更新签名内容
         signature.update(data);
-        //得到验证结果
+        // 得到验证结果
         return signature.verify(sign);
     }
 
